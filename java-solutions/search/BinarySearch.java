bde4532b417731f164deb05dc9df3db3eb0987cb
GrigoriyKarpuhin <grischa.karpukhin@yandex.ru>
2023-03-03 15:28:31 +0300

    Homework 2 fixed

--- java-solutions/search/BinarySearch.java
+++ java-solutions/search/BinarySearch.java
@@ -1,64 +1,62 @@
 package search;
 
-import java.util.ArrayList;
-
 public class BinarySearch {
-    //Pred: x ∈ int && a ∈ ArrayList<Integer> && ∀i: 0 <= i < a.size() -> a.get(i) ∈ int
-    //Post: result: a.get(result) <= x < a.get(result - 1)
+    //Pred: x ∈ int && a ∈ int[] && ∀i: 0 <= i < a.length -> a[i] ∈ int
+    //Post: result: a[result] <= x < a[result - 1]
     public static void main(String[] args) {
         int x = Integer.parseInt(args[0]);
-        ArrayList<Integer> a = new ArrayList<>();
+        int[] a = new int[args.length - 1];
         for (int i = 1; i < args.length; i++) {
-            a.add(Integer.parseInt(args[i]));
+            a[i - 1] = Integer.parseInt(args[i]);
         }
         int sum = 0;
-        for (int i = 0; i < a.size(); i++) {
-            sum += a.get(i);
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
         }
         if (sum % 2 == 0) {
-            System.out.println(recursiveBinarySearch(x, a, 0, a.size() - 1));
+            System.out.println(recursiveBinarySearch(x, a, 0, a.length - 1));
         } else {
             System.out.println(iterativeBinarySearch(x, a));
         }
     }
 
-    // Pred: ∀i: 0 <= i < a.size() -> a.get(i) >= a.get(i + 1)
-    // Post: result: a.get(result) <= x < a.get(result - 1)
-    private static int recursiveBinarySearch(int x, ArrayList<Integer> a, int left, int right) {
+    // Pred: ∀i: 0 <= i < a.length -> a[i] >= a[i + 1] && left < right
+    // Post: result: a[result] <= x < a[result - 1]
+    private static int recursiveBinarySearch(int x, int[] a, int left, int right) {
         if (left > right) {
             //left > right -> left == right + 1 == result
             return left;
         }
         //left <= right && mid == (left + right) / 2 -> left <= mid <= right
         int mid = (left + right) / 2;
-        if (x < a.get(mid)) {
-            //x < a.get(mid)
+        if (x < a[mid]) {
+            //x < a[mid]
             return recursiveBinarySearch(x, a, mid + 1, right);
             // left' == (left + right) / 2 + 1 && left' <= result
         } else {
-            //x >= a.get(mid)
+            //x >= a[mid]
             return recursiveBinarySearch(x, a, left, mid - 1);
             // right' == (left + right) / 2 - 1 && result < right'
         }
     }
 
-    // Pred: I && ∀i: 0 <= i < a.size() -> a.get(i) >= a.get(i + 1)
-    // Post: result: a.get(result) <= x < a.get(result - 1)
-    private static int iterativeBinarySearch(int x, ArrayList<Integer> a) {
-        // 0 <= 0 <= result <= a.size() - 1
+    // Pred: I && ∀i: 0 <= i < a.length -> a[i] >= a[i + 1]
+    // Post: result: a[result] <= x < a[result - 1]
+    private static int iterativeBinarySearch(int x, int[] a) {
+        // 0 <= 0 <= result <= a.length - 1
         int left = 0;
-        // 0 <= left <= result <= a.size() <= a.size()
-        int right = a.size() - 1;
-        // I: 0 <= left <= result <= right + 1 <= a.size()
+        // 0 <= left <= result <= a.length <= a.length
+        int right = a.length - 1;
+        // I: 0 <= left <= result <= right + 1 <= a.length
         while (left <= right) {
             // I && left <= right && mid == (left + right) / 2 -> left <= mid <= right
             int mid = (left + right) / 2;
-            if (x < a.get(mid)) {
-                // I && left <= right && x < a.get(mid)
+            if (x < a[mid]) {
+                // I && left <= right && x < a[mid]
                 left = mid + 1;
                 // left' == (left + right) / 2 + 1 && left' <= result
             } else {
-                // I && left <= right && x >= a.get(mid)
+                // I && left <= right && x >= a[mid]
                 right = mid - 1;
                 // right' == (left + right) / 2 - 1 && result < right'
             }
--- java-solutions/search/BinarySearchShift.java
+++ java-solutions/search/BinarySearchShift.java
@@ -1,42 +1,41 @@
 package search;
 
-import java.util.ArrayList;
-
 public class BinarySearchShift {
-    //Pred: a ∈ ArrayList<Integer> && ∀i: 0 <= i < a.size() -> a.get(i) ∈ int
+    //Pred: a ∈ int[] && ∀i: 0 <= i < a.length -> a[i] ∈ int
     //Post: result: a.get(result - 1) < a.get(result)
     public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<>();
+        int[] a = new int[args.length];
         for (int i = 0; i < args.length; i++) {
-            a.add(Integer.parseInt(args[i]));
+            a[i] = Integer.parseInt(args[i]);
         }
         int sum = 0;
-        for (int i = 0; i < a.size(); i++) {
-            sum += a.get(i);
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
         }
         if (sum % 2 == 0) {
-            System.out.println(recursiveBinarySearch(a, 0, a.size() - 1));
+            System.out.println(recursiveBinarySearch(a, 0, a.length - 1));
         } else {
             System.out.println(iterativeBinarySearch(a));
-       }
+        }
     }
-    // Pred: I && ∀i: 0 <= i < k -> a.get(i) > a.get(i + 1) && ∀i: k <= i < a.size() -> a.get(i) > a.get(i + 1) && a.get(k - 1) < a.get(k)
+
+    // Pred: I && ∀i: 0 <= i < k -> a[i] > a[i + 1] && ∀i: k <= i < a.length -> a[i] > a[i + 1] && a[k - 1] < a[k]
     // Post: result: a.get(result - 1) < a.get(result)
-    private static int iterativeBinarySearch(ArrayList<Integer> a) {
-        // 0 <= 0 <= result <= a.size() - 1
+    private static int iterativeBinarySearch(int[] a) {
+        // 0 <= 0 <= result <= a.length - 1
         int left = 0;
-        // 0 <= left <= result <= a.size() - 1
-        int right = a.size() - 1;
-        // I: 0 <= left <= result <= right <= a.size() - 1
+        // 0 <= left <= result <= a.length - 1
+        int right = a.length - 1;
+        // I: 0 <= left <= result <= right <= a.length - 1
         while (left < right) {
             // I && left < right && mid == (left + right) / 2 -> left <= mid < right
             int mid = (left + right) / 2;
-            if (a.get(mid) < a.get(right)) {
-                // I && left < right && a.get(mid) < a.get(right)
+            if (a[mid] < a[right]) {
+                // I && left < right && a[mid] < a[right]
                 left = mid + 1;
                 // left' == (left + right) / 2 + 1 && left' <= result
             } else {
-                // I && left < right && a.get(mid) > a.get(right)
+                // I && left < right && a[mid] > a[right]
                 right = mid;
                 // right' == (left + right) / 2 - 1 && result <= right'
             }
@@ -46,18 +45,18 @@ public class BinarySearchShift {
         return left;
     }
 
-    // Pred: ∀i: 0 <= i < k -> a.get(i) > a.get(i + 1) && ∀i: k <= i < a.size() -> a.get(i) > a.get(i + 1) && a.get(k - 1) < a.get(k)
+    // Pred: ∀i: 0 <= i < k -> a[i] > a[i + 1] && ∀i: k <= i < a.length -> a[i] > a[i + 1] && a[k - 1] < a[k] && left < right
     // Post: result: a.get(result - 1) < a.get(result)
-    private static int recursiveBinarySearch(ArrayList<Integer> a, int left, int right) {
+    private static int recursiveBinarySearch(int[] a, int left, int right) {
         if (left < right) {
             //left < right && mid == (left + right) / 2 -> left <= mid < right
             int mid = (left + right) / 2;
-            if (a.get(mid) < a.get(right)) {
-                //left < right && a.get(mid) < a.get(right)
+            if (a[mid] < a[right]) {
+                //left < right && a[mid] < a[right]
                 return recursiveBinarySearch(a, mid + 1, right);
                 // left' == (left + right) / 2 + 1 && left' <= result
             } else {
-                //left < right && a.get(mid) > a.get(right)
+                //left < right && a[mid] > a[right]
                 return recursiveBinarySearch(a, left, mid);
                 // right' == (left + right) / 2 - 1 && result <= right'
             }
