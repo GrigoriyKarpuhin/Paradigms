c669639cdd650305f4d18b52d6b41275e46f3149
GrigoriyKarpuhin <grischa.karpukhin@yandex.ru>
2023-04-20 13:40:20 +0300

    Homework 8

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -10,6 +10,9 @@ Const.prototype = {
     },
     toString: function () {
         return this.value.toString();
+    },
+    prefix: function () {
+        return this.value.toString();
     }
 }
 
@@ -30,6 +33,9 @@ Variable.prototype = {
     },
     toString: function () {
         return this.name;
+    },
+    prefix: function () {
+        return this.name;
     }
 }
 
@@ -44,11 +50,16 @@ const OperationCreate = function (operation, sign) {
         },
         toString: function () {
             return this.op.join(" ") + " " + sign;
+        },
+        prefix: function () {
+            return '(' + sign + ' ' + this.op.map(op => op.prefix()).join(" ") + ')'
         }
     }
+    mapOp[sign] = [Operation, operation.length];
     return Operation;
 }
 
+const mapOp = {};
 const Add = OperationCreate((a, b) => a + b, "+");
 const Subtract = OperationCreate((a, b) => a - b, "-");
 const Multiply = OperationCreate((a, b) => a * b, "*");
@@ -60,20 +71,11 @@ const Min3 = OperationCreate((...op) => Math.min(...op), "min3");
 const Max5 = OperationCreate((...op) => Math.max(...op), "max5");
 const Exp = OperationCreate(a => Math.exp(a), "exp");
 const Ln = OperationCreate(a => Math.log(a), "ln");
-
-const mapOp = {
-    "+": [Add, 2],
-    "-": [Subtract, 2],
-    "*": [Multiply, 2],
-    "/": [Divide, 2],
-    "negate": [Negate, 1],
-    "sinh": [Sinh, 1],
-    "cosh": [Cosh, 1],
-    "min3": [Min3, 3],
-    "max5": [Max5, 5],
-    "exp": [Exp, 1],
-    "ln": [Ln, 1]
-}
+const Sum = OperationCreate((...args) => args.reduce((a, b) => a + b, 0), "sum");
+const Avg = OperationCreate((...args) => {
+    const sum = args.reduce((a, b) => a + b, 0);
+    return sum / args.length;
+}, "avg");
 
 const mapVar = {
     "x": new Variable("x"),
@@ -100,4 +102,111 @@ const parse = function (expression) {
         }
     }
     return stack.pop();
+}
+
+function InvalidExpressionError(message) {
+    this.message = message;
+}
+
+InvalidExpressionError.prototype = Object.create(Error.prototype);
+InvalidExpressionError.prototype.name = "InvalidExpression";
+InvalidExpressionError.prototype.constructor = InvalidExpressionError;
+
+const validChecker = function (expressionPref) {
+    try {
+        const tokens = expressionPref.replace(/\)/g, " ) ").replace(/\(/g, " ( ").trim().split(" ");
+        if (expressionPref.replace(/[(, )]/gi, " ").trim().length === 0) {
+            throw new InvalidExpressionError("Empty expression");
+        }
+        let balance = 0;
+        let bracketFlag = false;
+        for (let token of tokens) {
+            if (token === "") continue;
+            if (bracketFlag === true) {
+                if (token in mapOp) {
+                    bracketFlag = false;
+                } else {
+                    throw new InvalidExpressionError("extra brackets");
+                }
+            }
+            if (token === '(') {
+                balance++;
+                bracketFlag = true;
+            }
+            if (token === ')') balance--;
+            if (balance < 0) {
+                throw new InvalidExpressionError("Wrong bracket sequence");
+            }
+        }
+        if (balance !== 0) {
+            throw new InvalidExpressionError("Wrong bracket sequence");
+        }
+    } catch (e) {
+        throw(e);
+    }
+}
+const parsePrefix = function (expressionPref) {
+    try {
+        validChecker(expressionPref);
+        const tokens = expressionPref.replace(/\)/g, " ) ").replace(/\(/gi, " ( ").trim().split(" ").reverse();
+        const stack = [];
+        let operFlag = false;
+        for (let token of tokens) {
+            if (token === "") continue;
+            if (token in mapVar) {
+                if (operFlag === true) {
+                    throw new InvalidExpressionError("extra operation");
+                }
+                stack.push(mapVar[token]);
+            } else if (token in mapOp) {
+                if (operFlag === true) {
+                    throw new InvalidExpressionError("extra operation");
+                }
+                operFlag = true;
+                const [operation, count] = mapOp[token];
+                const elements = [];
+                if (count !== 0) {
+                    if (stack.length < count + 1) {
+                        throw new InvalidExpressionError("extra operation");
+                    }
+                    let temp = 0;
+                    while (stack[stack.length - 1] !== ')' && stack.length > 0) {
+                        elements.push(stack.pop());
+                        temp++;
+                    }
+                    if (temp !== count) {
+                        throw new InvalidExpressionError("extra operation");
+                    }
+                    stack.pop();
+                    stack.push(new operation(...elements));
+                } else {
+                    while (stack[stack.length - 1] !== ')') {
+                        elements.push(stack.pop());
+                    }
+                    stack.pop();
+                    stack.push(new operation(...elements));
+                }
+            } else if (!isNaN(+token)) {
+                if (operFlag === true) {
+                    throw new InvalidExpressionError("extra operation");
+                }
+                stack.push(new Const(parseInt(token)));
+            } else if (token === ')') {
+                if (operFlag === true) {
+                    throw new InvalidExpressionError("extra operation");
+                }
+                stack.push(token);
+            } else if (token === '(') {
+                operFlag = false;
+            } else {
+                throw new InvalidExpressionError("invalid symbol " + token + " in " + expressionPref);
+            }
+        }
+        if (stack.length > 1) {
+            throw new InvalidExpressionError("extra operands");
+        }
+        return stack.pop();
+    } catch (e) {
+        throw(e);
+    }
 }
